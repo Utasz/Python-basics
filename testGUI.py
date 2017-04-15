175832903fbeb1c081f2d3c0720eae669a93ddb9
Utasz <lsloviq@gmail.com>
2017-04-16 01:04:44 +0200

    still testing commits from terminal. Added second button.

--- testGUI.py
+++ testGUI.py
@@ -1,8 +1,24 @@
 from Tkinter import *
 
+class App:
+
+	def __init__(self, master):
+		
+		frame = Frame(master)
+		frame.pack()
+
+		self.button = Button(frame, text = "QUIT", fg = "red", command = frame.quit)
+		self.button.pack(side = LEFT)
+
+		self.hi_there = Button(frame, text = "HELLO", command = self.say_hi)
+		self.hi_there.pack(side = LEFT)
+
+	def say_hi(self):
+		print "Hi there!"
+
 root = Tk()
 
-w = Label(root, text="Hello World!")
-w.pack()
+app = App(root)
 
 root.mainloop()
+root.destroy()
