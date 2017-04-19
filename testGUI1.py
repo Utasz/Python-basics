d5d63952a998e71bd1c5d769c6bb8c74a35c65b8
a <a@localhost.localdomain>
2017-04-19 21:23:46 +0200

    change text

--- testGUI1.py
+++ testGUI1.py
@@ -10,11 +10,11 @@ class App:
 		self.button = Button(frame, text = "QUIT", fg = "red", command = frame.quit)
 		self.button.pack(side = LEFT)
 
-		self.hi_there = Button(frame, text = "HI", command = self.say_hi)
+		self.hi_there = Button(frame, text = "HELLO", command = self.say_hi)
 		self.hi_there.pack(side = LEFT)
 
 	def say_hi(self):
-		print "Hi there!"
+		print "Hello there!"
 
 root = Tk()
 
